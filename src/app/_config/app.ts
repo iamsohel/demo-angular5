4cfc165f3131363065d0f19a8f196a03f13301c3
Sohel Rana <sohel9001@yahoo.com>
2018-04-03 12:26:58 +0600

    config file update

--- src/app/_config/app.ts
+++ src/app/_config/app.ts
@@ -7,8 +7,6 @@ export class AppConfig {
     { name: 'Test Company 5' },
     { name: 'Test Company 6' },
   ];
-  apiEndPoint = 'http://apicheckdefense.leapinglogic.com/';
-  graphClientID = '71f27613-0959-4efa-8440-9ee24211e19a';
-  graphEndpoint = 'https://graph.microsoft.com/v1.0/me';
-  graphScopes = ['Files.Read User.Read Mail.send'];
+  apiEndPoint = 'http://demo.com/api/';
+
 }
